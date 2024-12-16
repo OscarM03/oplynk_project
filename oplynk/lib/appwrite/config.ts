49cddc7a64d4c1b3cb021083c7a240da0993ef8f
OscarM03 <oscarmutwiri03@gmail.com>
2024-12-16 23:48:57 +0300

    Added more configs from the env, such as the chats collection id, gifts collection id, ....

--- oplynk/lib/appwrite/config.ts
+++ oplynk/lib/appwrite/config.ts
@@ -1,7 +1,13 @@
 export const appwriteconfig = {
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  imageEndpointUrl: process.env.NEXT_PUBLIC_APPWRITE_IMAGE_ENDPOINT!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
   usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+  sessionsCollectionId: process.env.NEXT_PUBLIC_APPWRITE_SESSIONS_COLLECTION!,
+  chatsCollectionId: process.env.NEXT_PUBLIC_APPWRITE_CHATS_COLLECTION!,
+  giftsCollectionId: process.env.NEXT_PUBLIC_APPWRITE_GIFTS_COLLECTION!,
+  bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+  giftsBucketId: process.env.NEXT_PUBLIC_APPWRITE_GIFTS_BUCKET!,
   secretKey: process.env.NEXT_APPWRITE_KEY!,
 }
