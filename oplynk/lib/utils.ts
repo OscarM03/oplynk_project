8a0d03cb2e47233918c7b924e9430f26d5333b01
OscarM03 <oscarmutwiri03@gmail.com>
2024-12-16 23:49:54 +0300

    Created a function to return the url of an image from the appwrite storage

--- oplynk/lib/utils.ts
+++ oplynk/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { appwriteconfig } from "./appwrite/config";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -7,3 +8,13 @@ export function cn(...inputs: ClassValue[]) {
 
 export const parseStringify = (value: unknown) => 
   JSON.parse(JSON.stringify(value));
+
+export const imageUrl = (bucketId: string, fileId: string) => {
+  return `${appwriteconfig.imageEndpointUrl}${bucketId}/files/${fileId}/view?project=${appwriteconfig.projectId}&project=${appwriteconfig.projectId}&mode=admin`;
+}
+
+export const formatter = new Intl.NumberFormat('en-US', {
+  style: 'decimal',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
