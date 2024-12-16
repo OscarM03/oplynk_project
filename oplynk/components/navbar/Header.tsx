8ee0d85f18b5b349513d2bd385c11f3d627e9c65
OscarM03 <oscarmutwiri03@gmail.com>
2024-12-16 23:45:37 +0300

    Added new links on the nacbar, Sessions and Gifts Library

--- oplynk/components/navbar/Header.tsx
+++ oplynk/components/navbar/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
 	return (
 		<header className="hidden sm:flex justify-between items-center sm:px-8 lg:px-12 py-2 shadow-md bg-white">
 			<div>
-				<h1 className="text-2xl font-extrabold textcolor">Oplynk</h1>
+				<Link href="/"> <h1 className="text-2xl font-extrabold textcolor">Oplynk</h1></Link>
 			</div>
 			<nav>
 				<ul className="flex space-x-6 ">
