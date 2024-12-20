efdca83099cacd5fe6c8f72be08d580b45d14f3b
OscarM03 <oscarmutwiri03@gmail.com>
2024-12-20 18:49:51 +0300

    Removing the navlinks from the header

--- oplynk/components/navbar/Header.tsx
+++ oplynk/components/navbar/Header.tsx
@@ -6,16 +6,6 @@ import React from "react";
 import NavAction from "./NavAction";
 import { useState, useEffect } from "react";
 
-const navLinks = [
-	{
-		label: "Sessions",
-		href: "/sessions",
-	},
-	{
-		label: "Gifts Library",
-		href: "/gifts",
-	}
-]
 
 const Header = () => {
 
@@ -39,17 +29,6 @@ const Header = () => {
 			<div>
 				<Link href="/"> <h1 className="text-2xl font-extrabold textcolor">Oplynk</h1></Link>
 			</div>
-			<nav>
-				<ul className="flex space-x-6 ">
-					{navLinks.map((link, index) => (
-						<li key={index} className="text-gray-600 hover:text-dodger-blue">
-							<Link href={link.href}>
-							 	<h2>{link.label}</h2>
-							</Link>
-						</li>
-					))}
-				</ul>
-			</nav>
 			<NavAction loggedIn={loggedIn} user={user}/>
 		</header>
 	);
