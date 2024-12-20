3bab1a2a141721b09cf88ab6242f8d3c7c832760
OscarM03 <oscarmutwiri03@gmail.com>
2024-12-20 11:25:57 +0300

    Added a function to add a gift to the receiver once sent

--- oplynk/lib/actions/gifts.action.ts
+++ oplynk/lib/actions/gifts.action.ts
@@ -3,6 +3,7 @@
 import { Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteconfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
 
 export const getGifts = async () => {
     const { databases } = await createAdminClient();
@@ -21,3 +22,36 @@ export const getGifts = async () => {
         throw new Error("Failed to get gifts")
     }
 }
+
+export const addGiftToReceiver = async (userId: string, giftId: string, giftValue: number) => {
+    const { databases } = await createAdminClient();
+
+    try {
+
+        const userDoc = await databases.getDocument(
+            appwriteconfig.databaseId,
+            appwriteconfig.usersCollectionId,
+            userId
+        );
+
+
+        const currentBalance = userDoc.balance || 0;
+        const updatedBalance = currentBalance + giftValue;
+
+
+        const updatedUser = await databases.updateDocument(
+            appwriteconfig.databaseId,
+            appwriteconfig.usersCollectionId,
+            userId,
+            {
+                balance: updatedBalance,
+            }
+        );
+
+        return parseStringify(updatedUser);
+    } catch (error) {
+        console.error("Error updating user with gift:", { userId, giftId, error });
+        throw new Error("Failed to update user with gift.");
+    }
+};
+
