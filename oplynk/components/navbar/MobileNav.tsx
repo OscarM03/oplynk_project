17bd124ed5d8e9bd8c0ee668817f63b9656c84a0
OscarM03 <oscarmutwiri03@gmail.com>
2024-12-20 11:15:30 +0300

    Added parallel fetching

--- oplynk/components/navbar/MobileNav.tsx
+++ oplynk/components/navbar/MobileNav.tsx
@@ -2,27 +2,34 @@
 
 import React from "react";
 import NavAction from "./NavAction";
-import {IsUserLoggedIn } from "@/lib/actions/user.actions";
+import {getCurrentUser, IsUserLoggedIn } from "@/lib/actions/user.actions";
 import { useState, useEffect } from "react";
 
 
 const MobileNav = () => {
 
 	const [loggedIn, setLoggedIn] = useState<boolean>(false);
-
-    useEffect(() => {
-        const fetchUserId = async () => {
-            setLoggedIn(await IsUserLoggedIn());
-        };
-        fetchUserId();
-    }, []);
+		const [user, setUser] = useState<any>()
+	
+		useEffect(() => {
+			const fetchUserId = async () => {
+				const [user, userLoginState] = await Promise.all([
+					getCurrentUser(),
+					IsUserLoggedIn()
+				]);
+				setUser(user)
+				setLoggedIn(userLoginState)
+			};
+			fetchUserId();
+		}, []);
+	
 	
 	return (
 		<section className="sm:hidden flex justify-between items-center px-4 py-2 shadow-md bg-white">
 			<div>
 				<h1 className="text-xl font-extrabold textcolor">Oplynk</h1>
 			</div>
-			<NavAction loggedIn={loggedIn} />
+			<NavAction loggedIn={loggedIn} user={user}/>
 		</section>
 	);
 };
