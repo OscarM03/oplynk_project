cfe05517f05ad3968ed521e6b23a650dbf7ab415
OscarM03 <oscarmutwiri03@gmail.com>
2024-12-20 18:53:49 +0300

    Adding the home link to the logo

--- oplynk/components/navbar/MobileNav.tsx
+++ oplynk/components/navbar/MobileNav.tsx
@@ -2,34 +2,35 @@
 
 import React from "react";
 import NavAction from "./NavAction";
-import {getCurrentUser, IsUserLoggedIn } from "@/lib/actions/user.actions";
+import { getCurrentUser, IsUserLoggedIn } from "@/lib/actions/user.actions";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 
 const MobileNav = () => {
 
 	const [loggedIn, setLoggedIn] = useState<boolean>(false);
-		const [user, setUser] = useState<any>()
-	
-		useEffect(() => {
-			const fetchUserId = async () => {
-				const [user, userLoginState] = await Promise.all([
-					getCurrentUser(),
-					IsUserLoggedIn()
-				]);
-				setUser(user)
-				setLoggedIn(userLoginState)
-			};
-			fetchUserId();
-		}, []);
-	
-	
+	const [user, setUser] = useState<any>()
+
+	useEffect(() => {
+		const fetchUserId = async () => {
+			const [user, userLoginState] = await Promise.all([
+				getCurrentUser(),
+				IsUserLoggedIn()
+			]);
+			setUser(user)
+			setLoggedIn(userLoginState)
+		};
+		fetchUserId();
+	}, []);
+
+
 	return (
 		<section className="sm:hidden flex justify-between items-center px-4 py-2 shadow-md bg-white">
 			<div>
-				<h1 className="text-xl font-extrabold textcolor">Oplynk</h1>
+				<Link href="/"> <h1 className="text-2xl font-extrabold textcolor">Oplynk</h1></Link>
 			</div>
-			<NavAction loggedIn={loggedIn} user={user}/>
+			<NavAction loggedIn={loggedIn} user={user} />
 		</section>
 	);
 };
